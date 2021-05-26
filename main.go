8da23b05b34f9a10089d2dbc52f8a5ca82b07354
zj611 <zhangjunbit@163.com>
2021-05-26 23:06:50 +0800

    update

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main()  {
-<<<<<<< HEAD
-	fmt.Println("master")
-=======
+
+	
 	fmt.Println("feature/dev")
->>>>>>> e984a4b09cea02f3fbe4637e121d8652240e2230
+        fmt.Println("feature/dev2")
 }
